7ebc5fc10f7d48bc8932eea1b3432946d6e91e0f
bambang0811 <bambangp0811@gmail.com>
2026-05-17 16:44:18 +0700

    fix cors render

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,17 @@
 import express from "express";
+import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
 
 const app = express();
 
+app.use(cors());
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Middleware
 app.use(express.json());
 
-// Static uploads
 app.use(
   "/uploads",
   express.static(path.join(__dirname, "../public/uploads"))
