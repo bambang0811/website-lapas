5ee76b62ca55afce5cd358e57c4cfd16a04b7dc5
bambang0811 <bambangp0811@gmail.com>
2026-05-17 16:33:32 +0700

    fix render server

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,9 +7,24 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// Middleware
 app.use(express.json());
 
+// Static uploads
 app.use(
   "/uploads",
   express.static(path.join(__dirname, "../public/uploads"))
 );
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Backend running...");
+});
+
+// PORT
+const PORT = process.env.PORT || 5000;
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
