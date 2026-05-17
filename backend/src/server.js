48221b0f91d447c95d7df4ff46b848a16cd83ee1
bambang0811 <bambangp0811@gmail.com>
2026-05-17 16:21:36 +0700

    fix upload static path

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,8 @@
 app.use(express.json());
 
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use(
+  "/uploads",
+  express.static(path.join(__dirname, "public/uploads"))
+);
 
 app.use("/api/berita", beritaRoutes);
