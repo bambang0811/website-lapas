c2bdef0a4ad0d1f5cc3908562e5e53dbfd132ef3
bambang0811 <bambangp0811@gmail.com>
2026-05-17 16:27:40 +0700

    fix app undefined

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,15 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(express.json());
 
 app.use(
   "/uploads",
-  express.static(path.join(__dirname, "public/uploads"))
+  express.static(path.join(__dirname, "../public/uploads"))
 );
-
-app.use("/api/berita", beritaRoutes);
